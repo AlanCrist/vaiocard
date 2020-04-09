fca43fb27dc734fcbffa26008c4b68bc885419e5
Alan <lanzn16@outlook.com>
2020-04-09 00:14:46 -0300

    add dark theme option

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,14 +16,18 @@ import FinancesScreen from './src/Finances';
 import MoreScreen from './src/More';
 
 export default function App() {
+  const scheme = useColorScheme();
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Dash" component={DashScreen} />
-        <Tab.Screen name="Transfers" component={TransfersScreen} />
-        <Tab.Screen name="Finances" component={FinancesScreen} />
-        <Tab.Screen name="More" component={MoreScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <AppearanceProvider>
+      <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Tab.Navigator initialRouteName="Dash">
+          <Tab.Screen name="Dash" component={DashScreen} />
+          <Tab.Screen name="Transfers" component={TransfersScreen} />
+          <Tab.Screen name="Finances" component={FinancesScreen} />
+          <Tab.Screen name="More" component={MoreScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 }
