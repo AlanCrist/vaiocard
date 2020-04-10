eeb21c90eb5a3753475a9948c30f16e8fcb537bc
Alan <lanzn16@outlook.com>
2020-04-10 20:46:51 -0300

    add utils

---
+++ src/Utils/index.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-shadow */
+/* eslint-disable func-names */
+/* eslint-disable prefer-spread */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-use-before-define */
+/* eslint-disable radix */
+
+export function getColorRandomList(num = 0, length = 8) {
+  function random(min, max, length) {
+    const numbers = [];
+
+    function _random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    Array.apply(null, new Array(length)).reduce(function (previous) {
+      let nextRandom;
+
+      if (previous === min) {
+        nextRandom = _random(min + 1, max);
+      } else if (previous === max) {
+        nextRandom = _random(min, max - 1);
+      } else if (_random(0, 1)) {
+        nextRandom = _random(previous + 1, max);
+      } else {
+        nextRandom = _random(min, previous - 1);
+      }
+
+      numbers.push(nextRandom);
+      return nextRandom;
+    }, _random(min, max));
+
+    return numbers;
+  }
+
+  const radnums = random(0, 5, length);
+
+  const generateColor = (index) => {
+    const list = [
+      ["#f20000", "#ff4a4d", "#ff9393"],
+      ["#00899f", shadeColor("#00899f", 40), shadeColor("#00899f", 40)],
+      ["#ff3e66", shadeColor("#ff3e66", 80), "#fdeaac"],
+      ["#8b16f8", "#649bf2", "#4ac8e5"],
+      ["#069444", "#85c232", "#f7ec22"],
+      ["#06aac3", "#85d5a0", "#f7fd81"],
+      ["#322a6c", "#5b2b7e", "#832d90"],
+      ["#ee2324", "#f48323", "#fbe721"],
+    ];
+    return list[radnums[index]];
+  };
+
+  return generateColor(num);
+}
+
+export function shadeColor(color, percent) {
+  let R = parseInt(color.substring(1, 3), 16);
+  let G = parseInt(color.substring(3, 5), 16);
+  let B = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt((R * (100 + percent)) / 100);
+  G = parseInt((G * (100 + percent)) / 100);
+  B = parseInt((B * (100 + percent)) / 100);
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  const RR =
+    R.toString(16).length === 1 ? `0${R.toString(16)}` : R.toString(16);
+  const GG =
+    G.toString(16).length === 1 ? `0${G.toString(16)}` : G.toString(16);
+  const BB =
+    B.toString(16).length === 1 ? `0${B.toString(16)}` : B.toString(16);
+
+  return `#${RR}${GG}${BB}`;
+}
